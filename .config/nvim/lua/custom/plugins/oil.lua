ab1d550e8d9cc0abdb9d74ff54aa90774b6aa732
curiousignite <berkeerbagci@outlook.com>
2026-01-19 12:05:27 +0300

    Overall changes

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -11,6 +11,25 @@ return {
         show_hidden = true,
         skip_confirm_for_simple_edits = true,
       },
+      default_file_explorer = true,
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["g~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
+      -- Set to false to disable all of the above keymaps
+      use_default_keymaps = false,
     })
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
